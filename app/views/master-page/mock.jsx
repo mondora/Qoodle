37307389ecab407a1c036ac045fb13df10397f64
jab <jab@localhost.localdomain>
2017-04-24 12:56:22 +0200

    bozza tabella

--- app/views/master-page/mock.jsx
+++ app/views/master-page/mock.jsx
@@ -24,12 +24,66 @@ export default class Elenco extends Component {
         };
     }
 
+
+    renderHeader()
+    {
+
+
+        return (
+            Object.keys(this.state.struct[0]).map( (e) => <td>{e}</td>)
+        );
+    }
+
+    renderBody()
+    {
+        var righe = [];
+        var riga =[];
+        var obj ;
+
+
+        for(var i=0; i<this.state.struct.length; i++) {
+            obj = this.state.struct[i];
+            riga = (Object.keys(obj).map((e) => <td>{obj[e]}</td>));
+            righe.push(<tr>{riga }</tr>);
+        }
+
+        return righe;
+    }
+
+
+    renderTable()
+    {
+        return (<div>
+        <table className="responsive-table">
+            <caption>List of open Qoodles</caption>
+            <thead>
+            <tr>
+                {this.renderHeader()}
+            </tr>
+            </thead>
+            <tfoot>
+
+            </tfoot>
+            <tbody>
+            {this.renderBody()}
+
+            </tbody>
+        </table>
+            </div>
+        );
+
+    }
+
+
         render()
         {
            return ( <div>
+               {this.renderTable()}
                <div>{this.state.struct[0].titolo}</div>
                <div>{this.state.struct[0].descrizione}</div>
                <div>{this.state.struct[0].total_response}</div>
+               <div>{"buongiorno"}</div>
+               <div>{this.state.struct.length}</div>
             </div>);
         }
 

a9a57d423eb4420a4889ca6a76edbbc124bba7a6
jab <jab@localhost.localdomain>
2017-05-12 09:49:18 +0200

    rename QVHeaderTable into QoodleHeaderTable

---
+++ app/components/QoodleHeaderTable.jsx
@@ -0,0 +1,29 @@
+import React, {Component, PropTypes} from "react";
+import ReactDOM from "react-dom";
+
+
+class QoodleHeaderTable extends Component {
+
+    constructor(props)
+    {
+        super(props);
+
+    }
+
+
+    render() {
+        var headerItem = [];
+        var colonne=this.props.struct;
+        console.log('props attese',this.props);
+        console.log('struct',this.props.struct);
+
+        for (var i = 0; i < colonne.length; i++) {
+            headerItem.push(<th scope="col">{ colonne[i].name }</th>)
+        }
+
+
+        return (<tr>{headerItem}</tr>);
+    }
+
+}
+export default QoodleHeaderTable;

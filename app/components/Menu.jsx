933a72a7766b137f3fcf73dbb9ae1b83edebb25c
jab <jab@localhost.localdomain>
2017-08-07 17:29:38 +0200

    modificate dimensioni e stile dei box nel menu

--- app/components/Menu.jsx
+++ app/components/Menu.jsx
@@ -16,18 +16,18 @@ export default class Menu extends Component {
   return this.props.isOpenMenu ? (
             <div className="menu" style={{ position:"absolute", top:"0px", backgroundColor:"#5888d6",  height: "100vh", width:"100vw", zIndex:"999", left:"0"}}>
                 <div style={{ textAlign: "center", margin: "calc((100vh - 200px - 40px) / 2 )"}}>
-                  <div style={{ backgroundColor:"white", display:"inline-block", width:"200px", height:"200px", margin:"20px", color:"#5888d6"}}>
+                  <div style={{ backgroundColor:"white", display:"inline-block", width:"200px", height:"200px", margin:"20px", color:"#5888d6", borderRadius: "5px"}}>
                   <div style={{height:"70%", padding:"10px"}}>
-                    <i className="fa fa-list-ol" aria-hidden="true" style={{fontSize:"70px"}}></i>
+                    <i className="fa fa-list-ol" aria-hidden="true" style={{fontSize:"70px", paddingTop:"30px"}}></i>
                   </div>
-                  <span style={{fontSize: "22px"}}>Qoodles List</span>
+                  <span style={{fontSize: "24px", fontWeight: "bold"}}>Qoodles List</span>
                     </div>
 
-                  <div style={{ backgroundColor:"white", display:"inline-block", width:"200px", height:"200px", margin:"20px", color:"#5888d6"}}>
+                  <div style={{ backgroundColor:"white", display:"inline-block", width:"200px", height:"200px", margin:"20px", color:"#5888d6",  borderRadius: "5px"}}>
                   <div style={{height:"70%", padding:"10px"}}>
-                    <i className="fa fa-pencil-square-o" aria-hidden="true" style={{fontSize:"70px"}}></i>
+                    <i className="fa fa-pencil-square-o" aria-hidden="true" style={{fontSize:"70px", paddingTop:"30px"}}></i>
                   </div>
-                  <span style={{fontSize: "22px"}}>New Qoodle</span>
+                  <span style={{fontSize: "24px", fontWeight: "bold"}}>New Qoodle</span>
                     </div>
                 </div>
             </div>
